fbb3245dc4b58f97db445bba6452ad0eb033b651
kevin <kevin,andresfontt@gmail.com>
2023-05-16 20:52:47 -0400

    ejercicio angular pildoras informaticas directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+export interface Entrada {
+  titulo: string;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de usuarios';
+  mensaje = ""
+  registrado=false;
+  nombre:string = "";
+  apellido:string = "";
+  cargo:string="";
+  entradas: Entrada[];
+
+  
+
+  constructor (){
+    this.entradas= [
+      {titulo:"Python cada dia mas presente"},
+      {titulo:"Java es muy potente"},
+      {titulo:"Me gusta mucho Angular"},
+      {titulo:"Tambien me gusta CSS"},
+      {titulo:"Como no gustarme Javascript"},
+
+      
+    ]
+  }
+
+  registrarUsuario() {
+
+    this.registrado=true
+    this.mensaje="usuario registrado con exito"
+  }
 }
